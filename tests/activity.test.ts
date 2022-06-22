5dd21c6b0fe70d6711d43ed98424b1b5ac25be92
nurodev <me@nuro.dev>
2022-06-22 17:46:51 +0100

    Minor activity test tweaks

--- tests/activity.test.ts
+++ tests/activity.test.ts
@@ -1,6 +1,18 @@
 import { describe, it, expect } from "vitest";
 
+import "dotenv/config";
+
+import { Client } from "../dist";
+
 describe("Activity", () => {
-  it("Client", () => expect(true).toEqual(true));
-  it("Function", () => expect(true).toEqual(true));
+  it("Client", async () => {
+    const _client = new Client({
+      apiKey: process.env.MAILERSEND_API_KEY,
+    });
+
+    expect(true).toEqual(true);
+  });
+  it("Function", () => {
+    expect(true).toEqual(true);
+  });
 });
