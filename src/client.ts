9c2616a36c08a735860d51a9e44f603d6352da93
nurodev <me@nuro.dev>
2022-06-22 20:02:17 +0100

    Minor code cleanup / improvements

--- src/client.ts
+++ src/client.ts
@@ -1,27 +1,4 @@
-import {
-  addDomain,
-  allTemplates,
-  bulkEmailStatus,
-  deleteDomain,
-  deleteScheduledMessage,
-  deleteTemplate,
-  getActivityByCountry,
-  getActivityByDate,
-  getActivityByReadingEnvironment,
-  getActivityByUserAgent,
-  getActivityList,
-  getDnsRecords,
-  getRecipientsForDomain,
-  getSingleDomain,
-  getVerificationStatus,
-  listDomains,
-  listScheduledMessages,
-  send,
-  sendBulk,
-  singleScheduledMessage,
-  templateById,
-  updateDomainSettings,
-} from "~/modules";
+import * as modules from "~/modules";
 
 import type {
   ActivityByCountryParams,
@@ -52,75 +29,75 @@ export class Client implements IClient {
   }
 
   public async activityList(options: ActivityListParams) {
-    return getActivityList(this._apiKey, options);
+    return modules.getActivityList(this._apiKey, options);
   }
   public async activityByCountry(options: ActivityByCountryParams) {
-    return getActivityByCountry(this._apiKey, options);
+    return modules.getActivityByCountry(this._apiKey, options);
   }
   public async activityByDate(options: ActivityByDateParams) {
-    return getActivityByDate(this._apiKey, options);
+    return modules.getActivityByDate(this._apiKey, options);
   }
   public async activityByReadingEnvironment(
     options: ActivityByReadingEnvironmentParams
   ) {
-    return getActivityByReadingEnvironment(this._apiKey, options);
+    return modules.getActivityByReadingEnvironment(this._apiKey, options);
   }
   public async activityByUserAgent(options: ActivityByUserAgentParams) {
-    return getActivityByUserAgent(this._apiKey, options);
+    return modules.getActivityByUserAgent(this._apiKey, options);
   }
 
   public async addDomain(options: AddDomainParams) {
-    return addDomain(this._apiKey, options);
+    return modules.addDomain(this._apiKey, options);
   }
   public async deleteDomain(options: DeleteDomainParams) {
-    return deleteDomain(this._apiKey, options);
+    return modules.deleteDomain(this._apiKey, options);
   }
   public async dnsRecords(options: DnsRecordsParams) {
-    return getDnsRecords(this._apiKey, options);
+    return modules.getDnsRecords(this._apiKey, options);
   }
   public async recipientsForDomain(options: RecipientsForDomainParams) {
-    return getRecipientsForDomain(this._apiKey, options);
+    return modules.getRecipientsForDomain(this._apiKey, options);
   }
   public async singleDomain(options: SingleDomainParams) {
-    return getSingleDomain(this._apiKey, options);
+    return modules.getSingleDomain(this._apiKey, options);
   }
   public async verificationStatus(options: VerificationStatusParams) {
-    return getVerificationStatus(this._apiKey, options);
+    return modules.getVerificationStatus(this._apiKey, options);
   }
   public async listDomains(options: ListDomainsParams) {
-    return listDomains(this._apiKey, options);
+    return modules.listDomains(this._apiKey, options);
   }
   public async updateDomainSettings(options: UpdateDomainSettingsParams) {
-    return updateDomainSettings(this._apiKey, options);
+    return modules.updateDomainSettings(this._apiKey, options);
   }
 
   public async bulkEmailStatus(bulkEmailId: string) {
-    return bulkEmailStatus(this._apiKey, bulkEmailId);
+    return modules.bulkEmailStatus(this._apiKey, bulkEmailId);
   }
   public async send(options: SendParams) {
-    return send(this._apiKey, options);
+    return modules.send(this._apiKey, options);
   }
   public async sendBulk(options: Array<SendBulkParams>) {
-    return sendBulk(this._apiKey, options);
+    return modules.sendBulk(this._apiKey, options);
   }
 
   public async deleteScheduledMessage(messageId: string) {
-    return deleteScheduledMessage(this._apiKey, messageId);
+    return modules.deleteScheduledMessage(this._apiKey, messageId);
   }
   public async listScheduledMessages(options: ListScheduledMessagesParams) {
-    return listScheduledMessages(this._apiKey, options);
+    return modules.listScheduledMessages(this._apiKey, options);
   }
   public async singleScheduledMessage(messageId: string) {
-    return singleScheduledMessage(this._apiKey, messageId);
+    return modules.singleScheduledMessage(this._apiKey, messageId);
   }
 
   public async allTemplates(options: AllTemplatesParams) {
-    return allTemplates(this._apiKey, options);
+    return modules.allTemplates(this._apiKey, options);
   }
   public async deleteTemplate(templateId: string) {
-    return deleteTemplate(this._apiKey, templateId);
+    return modules.deleteTemplate(this._apiKey, templateId);
   }
   public async templateById(templateId: string) {
-    return templateById(this._apiKey, templateId);
+    return modules.templateById(this._apiKey, templateId);
   }
 }
