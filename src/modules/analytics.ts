7b2938786ccd756001065a1f7ffe750cfee09be7
nurodev <me@nuro.dev>
2022-06-22 18:03:38 +0100

    Added initial analytics module

---
+++ src/modules/analytics.ts
@@ -0,0 +1,199 @@
+import { fetch } from "~/util";
+
+import type { GroupBy } from "~/types";
+
+export interface ActivityByDateParams extends Record<string, any> {
+  domain_id?: string;
+  /**
+   * Not yet implemented
+   *
+   * Max number of recipients: `50`
+   *
+   * *TBD*
+   */
+  recipient_id?: string;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be lower than date_to.
+   */
+  date_from: number;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be higher than date_from.
+   */
+  date_to: number;
+  /**
+   * @default days
+   */
+  group_by?: GroupBy;
+  tags?: Array<string>;
+  event: Array<Event>;
+}
+
+/**
+ * Get Activity By Date
+ *
+ * @description Retrieve data grouped by date, based on activity
+ *
+ * @see https://developers.mailersend.com/api/v1/analytics.html#activity-data-by-date
+ *
+ * @param {String} apiKey - Unique API access token
+ * @param {Object} options - Activity list options
+ */
+export async function getActivityByDate<TResponse = Record<string, any>>(
+  apiKey: string,
+  { domainId, ...params }: ActivityByDateParams
+): Promise<TResponse> {
+  return fetch({
+    apiKey,
+    endpoint: "/analytics/date",
+    method: "GET",
+    params,
+  });
+}
+
+export interface ActivityByCountryParams extends Record<string, any> {
+  domain_id?: string;
+  /**
+   * Not yet implemented
+   *
+   * Max number of recipients: `50`
+   *
+   * *TBD*
+   */
+  recipient_id?: string;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be lower than date_to.
+   */
+  date_from: number;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be higher than date_from.
+   */
+  date_to: number;
+  tags?: Array<string>;
+}
+
+/**
+ * Get Activity By Country
+ *
+ * @description Retrieve data grouped by country, based on activity
+ *
+ * @see https://developers.mailersend.com/api/v1/analytics.html#opens-by-country
+ *
+ * @param {String} apiKey - Unique API access token
+ * @param {Object} options - Activity list options
+ */
+export async function getActivityByCountry<TResponse = Record<string, any>>(
+  apiKey: string,
+  params: ActivityByCountryParams
+): Promise<TResponse> {
+  return fetch({
+    apiKey,
+    endpoint: "/analytics/country",
+    method: "GET",
+    params,
+  });
+}
+
+export interface ActivityByUserAgentParams extends Record<string, any> {
+  domain_id?: string;
+  /**
+   * Not yet implemented
+   *
+   * Max number of recipients: `50`
+   *
+   * *TBD*
+   */
+  recipient_id?: string;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be lower than date_to.
+   */
+  date_from: number;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be higher than date_from.
+   */
+  date_to: number;
+  tags?: Array<string>;
+}
+
+/**
+ * Activity Get By User Agent
+ *
+ * @description Retrieve data grouped by user-agent name (browser and operating system), based on activity
+ *
+ * @see https://developers.mailersend.com/api/v1/analytics.html#opens-by-user-agent-name
+ *
+ * @param {String} apiKey - Unique API access token
+ * @param {Object} options - Activity list options
+ */
+export async function getActivityByUserAgent<TResponse = Record<string, any>>(
+  apiKey: string,
+  params: ActivityByUserAgentParams
+): Promise<TResponse> {
+  return fetch({
+    apiKey,
+    endpoint: "/analytics/ua-name",
+    method: "GET",
+    params,
+  });
+}
+
+export interface ActivityByReadingEnvironmentParams
+  extends Record<string, any> {
+  domain_id?: string;
+  /**
+   * Not yet implemented
+   *
+   * Max number of recipients: `50`
+   *
+   * *TBD*
+   */
+  recipient_id?: string;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be lower than date_to.
+   */
+  date_from: number;
+  /**
+   * Timestamp is assumed to be UTC.
+   *
+   * Must be higher than date_from.
+   */
+  date_to: number;
+  tags?: Array<string>;
+}
+
+/**
+ * Activity Get By Reading Environment
+ *
+ * @description Retrieve data grouped by the reading environment (webmail, mobile, desktop), based on activity
+ *
+ * @see https://developers.mailersend.com/api/v1/analytics.html#opens-by-reading-environment
+ *
+ * @param {String} apiKey - Unique API access token
+ * @param {Object} options - Activity list options
+ */
+export async function getActivityByReadingEnvironment<
+  TResponse = Record<string, any>
+>(
+  apiKey: string,
+  params: ActivityByReadingEnvironmentParams
+): Promise<TResponse> {
+  return fetch({
+    apiKey,
+    endpoint: "/analytics/ua-type",
+    method: "GET",
+    params,
+  });
+}
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,58 +1,12 @@
-export { type ActivityListParams, getActivityList } from "./activity";
-// export { Activity, type ActivityListParams } from "./activity";
+export { getActivityList, type ActivityListParams } from "./activity";
 
-// export {
-//   Analytics,
-//   type ActivityByCountryParams,
-//   type ActivityByDateParams,
-//   type ActivityByReadingEnvironmentParams,
-//   type ActivityByUserAgentParams,
-// } from "./analytics";
-
-// export {
-//   Domain,
-//   type AddDomainParams,
-//   type DnsRecordsParams,
-//   type ListDomainsParams,
-//   type RecipientsForDomainParams,
-//   type SingleDomainParams,
-//   type UpdateDomainSettingsParams,
-//   type VerificationStatusParams,
-// } from "./domains";
-
-// export { Email, type SendParams } from "./email";
-
-// export {
-//   InboundRouting,
-//   type AddInboundRouteParams,
-//   type ListInboundRoutesParams,
-//   type UpdateInboundRouteParams,
-// } from "./inboundRouting";
-
-// export { Messages, type ListMessagesParams } from "./messages";
-
-// export {
-//   ScheduledMessages,
-//   type ListScheduledMessagesParams,
-// } from "./scheduledMessages";
-
-// export {
-//   Recipients,
-//   type AddToSuppressionListParams,
-//   type DeleteFromSuppressionListParams,
-// } from "./recipients";
-
-// export { Templates, type AllTemplatesParams } from "./templates";
-
-// export {
-//   Tokens,
-//   type CreateTokenParams,
-//   type UpdateTokenParams,
-// } from "./tokens";
-
-// export {
-//   Webhooks,
-//   type CreateWebhookParams,
-//   type ListWebhoksParams,
-//   type UpdateWebhookParams,
-// } from "./webhooks";
+export {
+  getActivityByCountry,
+  getActivityByDate,
+  getActivityByReadingEnvironment,
+  getActivityByUserAgent,
+  type ActivityByCountryParams,
+  type ActivityByDateParams,
+  type ActivityByReadingEnvironmentParams,
+  type ActivityByUserAgentParams,
+} from "./analytics";
