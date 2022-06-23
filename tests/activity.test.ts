c1ccdbcae358b62ff4cd1701f8b6e5b1a3d62aaa
nurodev <me@nuro.dev>
2022-06-23 04:11:41 +0100

    Added functioning activity tests

--- tests/activity.test.ts
+++ tests/activity.test.ts
@@ -2,18 +2,39 @@ import { describe, it, expect } from "vitest";
 
 import "dotenv/config";
 
-import { Client } from "../dist";
+import { Client, activityList } from "../dist";
+
+const { MAILERSEND_API_KEY, MAILERSEND_DOMAIN_ID } = process.env;
 
 describe("Activity", () => {
   it.concurrent("Client", async () => {
-    const _client = new Client({
-      apiKey: process.env.MAILERSEND_API_KEY,
+    const client = new Client({
+      apiKey: MAILERSEND_API_KEY as string,
     });
 
-    expect(true).toEqual(true);
+    try {
+      const activityListResponse = await client.activityList({
+        domainId: MAILERSEND_DOMAIN_ID as string,
+      });
+
+      expect(activityListResponse).not.toBeNull();
+    } catch (error) {
+      console.error(error);
+    }
   });
 
-  it.concurrent("Function", () => {
-    expect(true).toEqual(true);
+  it.concurrent("Function", async () => {
+    try {
+      const activityListResponse = await activityList(
+        MAILERSEND_API_KEY as string,
+        {
+          domainId: MAILERSEND_DOMAIN_ID as string,
+        }
+      );
+
+      expect(activityListResponse).not.toBeNull();
+    } catch (error) {
+      console.error(error);
+    }
   });
 });
