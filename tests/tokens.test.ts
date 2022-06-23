396d1f0a70e193ca346fa4e7484742b01452efba
nurodev <me@nuro.dev>
2022-06-23 06:21:20 +0100

    Added initial functioning token tests

--- tests/tokens.test.ts
+++ tests/tokens.test.ts
@@ -2,26 +2,83 @@ import { describe, it, expect } from "vitest";
 
 import "dotenv/config";
 
-import { Client } from "../dist";
+import { Client, createToken, deleteToken } from "../dist";
 
 const { MAILERSEND_API_KEY, MAILERSEND_DOMAIN_ID } = process.env;
 
 describe("Tokens", () => {
-  it.concurrent("Client", async () => {
-    const client = new Client({
-      apiKey: MAILERSEND_API_KEY as string,
-    });
+  const client = new Client({
+    apiKey: MAILERSEND_API_KEY as string,
+  });
 
+  it.concurrent("Client - Create Token", async () => {
     try {
-      expect(true).not.toBeNull();
+      const createTokenResponse = await client.createToken({
+        domain_id: MAILERSEND_DOMAIN_ID as string,
+        name: "TEST_API_TOKEN",
+        scopes: [],
+      });
+
+      expect(createTokenResponse).not.toBeNull();
+      expect(createTokenResponse.data).not.toBeNull();
     } catch (error) {
       console.error(error);
     }
   });
 
-  it.concurrent("Function", async () => {
+  it.concurrent("Function - Create Token", async () => {
     try {
-      expect(true).toEqual(true);
+      const createTokenResponse = await createToken(
+        MAILERSEND_API_KEY as string,
+        {
+          domain_id: MAILERSEND_DOMAIN_ID as string,
+          name: "TEST_API_TOKEN",
+          scopes: [],
+        }
+      );
+
+      expect(createTokenResponse).not.toBeNull();
+      expect(createTokenResponse.data).not.toBeNull();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  it.concurrent("Client - Delete Token", async () => {
+    try {
+      const { data: newToken } = await client.createToken({
+        domain_id: MAILERSEND_DOMAIN_ID as string,
+        name: "TEST_DELETE_TOKEN",
+        scopes: [],
+      });
+
+      const deleteTokenResponse = await client.deleteToken(newToken.id);
+
+      expect(deleteTokenResponse).not.toBeNull();
+      expect(deleteTokenResponse.success).not.toBeNull();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  it.concurrent("Function - Delete Token", async () => {
+    try {
+      const { data: newToken } = await createToken(
+        MAILERSEND_API_KEY as string,
+        {
+          domain_id: MAILERSEND_DOMAIN_ID as string,
+          name: "TEST_DELETE_TOKEN",
+          scopes: [],
+        }
+      );
+
+      const deleteTokenResponse = await deleteToken(
+        MAILERSEND_API_KEY as string,
+        newToken.id
+      );
+
+      expect(deleteTokenResponse).not.toBeNull();
+      expect(deleteTokenResponse.success).not.toBeNull();
     } catch (error) {
       console.error(error);
     }
