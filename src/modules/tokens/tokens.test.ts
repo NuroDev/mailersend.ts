14e721a66c7148e91df811e1b3d066cec42b55ba
nurodev <me@nuro.dev>
2022-07-04 08:29:35 +0100

    Updated tests for tokens module

--- src/modules/tokens/tokens.test.ts
+++ src/modules/tokens/tokens.test.ts
@@ -4,49 +4,43 @@ import "dotenv/config";
 
 import { createToken, deleteToken } from ".";
 
-const { MAILERSEND_API_KEY, MAILERSEND_DOMAIN_ID } = process.env;
+const { MAILERSEND_API_KEY: apiKey, MAILERSEND_DOMAIN_ID: domain_id } =
+  process.env as Record<string, string>;
 
 describe("Tokens", () => {
   it.concurrent("Create Token", async () => {
     try {
-      const createTokenResponse = await createToken(
-        MAILERSEND_API_KEY as string,
-        {
-          domain_id: MAILERSEND_DOMAIN_ID as string,
-          name: "TEST_API_TOKEN",
-          scopes: [],
-        }
-      );
+      const createTokenResponse = await createToken(apiKey, {
+        domain_id,
+        name: "TEST_API_TOKEN",
+        scopes: [],
+      });
 
       expect(createTokenResponse).not.toBeNull();
       expect(createTokenResponse.data).not.toBeNull();
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   });
 
+  // TODO: Add test to update a token
+
   it.concurrent("Delete Token", async () => {
     try {
-      const { data: newToken } = await createToken(
-        MAILERSEND_API_KEY as string,
-        {
-          domain_id: MAILERSEND_DOMAIN_ID as string,
-          name: "TEST_DELETE_TOKEN",
-          scopes: [],
-        }
-      );
+      const { data: newToken } = await createToken(apiKey, {
+        domain_id,
+        name: "TEST_DELETE_TOKEN",
+        scopes: [],
+      });
 
       if (!newToken?.id) throw "No new token ID found.";
 
-      const deleteTokenResponse = await deleteToken(
-        MAILERSEND_API_KEY as string,
-        newToken.id
-      );
+      const deleteTokenResponse = await deleteToken(apiKey, newToken.id);
 
       expect(deleteTokenResponse).not.toBeNull();
       expect(deleteTokenResponse.success).not.toBeNull();
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   });
 });
