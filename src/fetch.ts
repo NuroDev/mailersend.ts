5a05140294b0b2ae3354b0a1b11c474701d0461b
nurodev <me@nuro.dev>
2022-07-04 08:03:10 +0100

    Added new WIP `fetch2` fetcher function

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,5 +1,7 @@
 import nodeFetch from "node-fetch";
 
+import type { Response } from "node-fetch";
+
 import type { RequestOptions } from "~/types";
 
 export async function fetch<TResponse = Record<string, any>>({
@@ -33,4 +35,37 @@ export async function fetch<TResponse = Record<string, any>>({
   const json = (await response.json()) as TResponse;
 
   return json;
+}
+
+export async function fetch2({
+  apiVersion = "v1",
+  apiKey,
+  basePath = "https://api.mailersend.com",
+  body = undefined,
+  endpoint,
+  headers = {},
+  method = "GET",
+  params = {},
+}: RequestOptions): Promise<Response> {
+  const url = new URL(`${apiVersion}${endpoint}`, basePath);
+  Object.entries(params).forEach(([key, value]) =>
+    url.searchParams.append(key, value)
+  );
+
+  const response = await nodeFetch(url.href, {
+    method,
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${apiKey}`,
+    },
+    ...(body !== undefined
+      ? {
+          body: typeof body === "string" ? body : JSON.stringify(body),
+        }
+      : {}),
+  });
+
+  if (!response.ok) throw await response.json();
+
+  return response;
 }
