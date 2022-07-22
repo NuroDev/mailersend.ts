108bf5d06f7919d37bde3dc37041cd441f14c054
nurodev <me@nuro.dev>
2022-07-22 01:08:54 +0100

    Added email module to client class

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,27 @@
-// import * as modules from "~/modules";
+import * as modules from "~/modules";
 
 import type { IClient } from "~/types/client";
 
 export class Client implements IClient {
-  // private _apiKey: string;
+  private _apiKey: string;
 
   /**
    * @see https://www.mailersend.com/help/managing-api-tokens
    *
    * @param {String} apiKey - API Access token
    */
-  constructor(_apiKey: string) {
-    // this._apiKey = apiKey;
+  constructor(apiKey: string) {
+    this._apiKey = apiKey;
+  }
+
+  // ------------------------------------------------------------
+  // Email
+  // ------------------------------------------------------------
+
+  public async sendEmail(options: modules.SendEmailParams) {
+    return modules.sendEmail(this._apiKey, options);
+  }
+  public async sendBulkEmails(options: Array<modules.SendBulkEmailsParams>) {
+    return modules.sendBulkEmails(this._apiKey, options);
   }
 }
--- src/types/client.ts
+++ src/types/client.ts
@@ -1,3 +1,41 @@
-// import * as modules from "~/modules";
+import * as modules from "~/modules";
 
-export interface IClient {}
+export interface IClient {
+  // ------------------------------------------------------------
+  // Email
+  // ------------------------------------------------------------
+
+  /**
+   * Send Email
+   *
+   * @description This endpoint allows you to send an asynchronous email.
+   *
+   * It returns the status of the email sent with an X-Message-Id that can be used to continuously query for the status using the Email API.
+   *
+   * @see https://developers.mailersend.com/api/v1/email.html#send-an-email
+   *
+   * @param {Object} options - Send email options
+   */
+  sendEmail(
+    options: modules.SendEmailParams
+  ): ReturnType<typeof modules.sendEmail>;
+
+  /**
+   * Send Bulk Emails
+   *
+   * @description This endpoint allows you to send multiple asynchronous emails.
+   *
+   * It returns the status of the request sent with a `bulk_email_id` that can be used to continuously query for the status using the Email API.
+   *
+   * To prevent long waiting periods for a response, each email validation is done after the request and then the result is stored.
+   *
+   * If there is any validation error, you can query it using the `bulk_email_id` provided.
+   *
+   * @see https://developers.mailersend.com/api/v1/email.html#send-bulk-emails
+   *
+   * @param {Object} options - Send bulk emails options
+   */
+  sendBulkEmails(
+    options: Array<modules.SendBulkEmailsParams>
+  ): ReturnType<typeof modules.sendBulkEmails>;
+}
