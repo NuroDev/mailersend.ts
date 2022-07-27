97fd3048313f9157e4700fac56d37e5e25b25623
nurodev <me@nuro.dev>
2022-07-27 21:29:25 +0100

    Updated `tsup` config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
-import type { Options } from "tsup";
+import { defineConfig } from "tsup";
 
 const isProduction = process.env.NODE_ENV === "production";
 
-export const tsup: Options = {
+export default defineConfig({
   clean: true,
   dts: true,
   entryPoints: {
     index: "src/index.ts",
     types: "src/types.ts",
   },
-  format: ["cjs", "esm", "iife"],
+  format: ["cjs", "esm"],
   minify: isProduction,
   sourcemap: true,
-};
+});
